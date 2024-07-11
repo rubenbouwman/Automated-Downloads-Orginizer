e79b352a56a0b7d37b75e7a6fd4c29c56e679295
Ruben Bouwman <rubensb2001@gmail.com>
2024-07-12 00:31:40 +0200

    Added new categories and fixed a bug
    Added categories "Other" and "Folders" and fixed a bug where folders were made in other folders, this was caused by the execution order of the code so I had to change the order of 2 sections of the code.

--- Program.py
+++ Program.py
@@ -1,6 +1,10 @@
 # --------------------------- Imports ---------------------------
 import os
 import shutil
+# --------------------------- Testing ---------------------------
+# Directory for a test folder, comment out 'filePath' below 
+# if you want to use this
+# filePath = r"./Downloads/Testfolder"
 
 # --------------------------- Setup ---------------------------
 # File path for the folder that needs to be organized
@@ -14,8 +18,13 @@ categories = {
     'Videos': ['.mp4', '.mkv', '.flv', '.avi', '.mov'],
     'Music': ['.mp3', '.wav', '.aac', '.flac'],
     'Archives': ['.zip', '.rar', '.tar', '.gz'],
-    'Executables': ['.exe']
+    'Executables': ['.exe'],
+    'Folders':[],
+    'Other':[]
 }
+
+# Make a list of all the files in the given directory
+fileList = [files for files in os.listdir(filePath)]
 
 # check if category folders exist, make the folder if it doesn't
 for folder in categories:
@@ -23,26 +32,25 @@ for folder in categories:
     if not os.path.exists(folderPath):
         os.makedirs(folderPath)
 
-# Make a list of all the files in the given directory
-fileList = [files for files in os.listdir(filePath)]
-
 # --------------------------- Functions ---------------------------
 # Function to determine the category based on the file extension
 def getCategory(file):
+    if os.path.isdir(os.path.join(filePath, file)):
+        return 'Folders'
+
     fileNames_Splitted = os.path.splitext(file)
     fileExtension = fileNames_Splitted[1].lower()
     for catagory, extensions in categories.items():
         if fileExtension in extensions:
             return catagory
-    return None
+    return "Other"
 
 # Function to move a file to a category folder
 def moveFile(file, category):
-    if category:
-        fileSourcePath = os.path.join(filePath, file)
-        destinationPath = os.path.join(filePath, category, file)
-        shutil.move(fileSourcePath, destinationPath)
-        print(f"Moved {file} to the {category} folder")
+    fileSourcePath = os.path.join(filePath, file)
+    destinationPath = os.path.join(filePath, category, file)
+    shutil.move(fileSourcePath, destinationPath)
+    print(f"Moved {file} to the {category} folder")
 
 # --------------------------- Execute ---------------------------
 # Organize files to their respective folders
@@ -50,3 +58,5 @@ for file in fileList:
     category = getCategory(file)
     print(f"moving {file} to {category}")
     moveFile(file, category)
+
+print("Finished organizing")
