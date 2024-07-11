faa3d8e6fc373bf05d6a86dc116003f0a4cbca2f
Ruben Bouwman <rubensb2001@gmail.com>
2024-07-12 01:21:23 +0200

    Optimised code and Fixed bugs

--- Program.py
+++ Program.py
@@ -6,6 +6,7 @@ import shutil
 # File path for the folder that needs to be organized
 # filePath = r"C:/Users/ubrub/Downloads"
 filePath = input("Enter the path to Downloads folder: ")
+fileList = []
 
 # Categories for the folders and their coresponding file types
 # New Categories and extensions can be added here
@@ -20,16 +21,20 @@ categories = {
     'Other':[]
 }
 
-# Make a list of all the files in the given directory
-fileList = [files for files in os.listdir(filePath)]
+# --------------------------- Functions ---------------------------
+# Function to make a list of all the files in the given directory
+def setFileList():
+    global fileList
+    fileList = [files for files in os.listdir(filePath)]
 
 # check if category folders exist, make the folder if it doesn't
-for folder in categories:
-    folderPath = os.path.join(filePath, folder)
-    if not os.path.exists(folderPath):
-        os.makedirs(folderPath)
+def makeFolders():
+    for folder in categories:
+        folderPath = os.path.join(filePath, folder)
+        if not os.path.exists(folderPath):
+            print("Making category folders")
+            os.makedirs(folderPath)
 
-# --------------------------- Functions ---------------------------
 # Function to determine the category based on the file extension
 def getCategory(file):
     if os.path.isdir(os.path.join(filePath, file)):
@@ -43,16 +48,21 @@ def getCategory(file):
     return "Other"
 
 # Function to move a file to a category folder
-def moveFile(file, category):
-    fileSourcePath = os.path.join(filePath, file)
-    destinationPath = os.path.join(filePath, category, file)
-    shutil.move(fileSourcePath, destinationPath)
-    print(f"Moved {file} to the {category} folder")
+def moveFile():
+    print("Organizing...")
+    for file in fileList:
+        category = getCategory(file)
+        fileSourcePath = os.path.join(filePath, file)
+        destinationPath = os.path.join(filePath, category, file)
+        shutil.move(fileSourcePath, destinationPath)
+        print(f"Moved {file} to the {category} folder")
+
+# Template method - Organize files to their respective folders
+def run():
+    setFileList()
+    makeFolders()
+    moveFile()
+    print("Finished organizing")
 
 # --------------------------- Execute ---------------------------
-# Organize files to their respective folders
-for file in fileList:
-    category = getCategory(file)
-    moveFile(file, category)
-
-print("Finished organizing")
+run()
