5296f9f1ede90849b76b59a208df91ac279987e3
Ruben Bouwman <rubensb2001@gmail.com>
2024-07-11 22:00:41 +0200

    Files are added to a list, added categories

--- Program.py
+++ Program.py
@@ -1,4 +1,18 @@
 import os
 
-for files in os.listdir(r"C:/Users/ubrub/Downloads"):
-  print(files)
+# File path for the folder that needs to be organized
+filePath = r"C:/Users/ubrub/Downloads"
+
+# Categories for the folders and their coresponding file types
+categories = {
+    'Documents': ['.pdf', '.doc', '.docx', '.txt', '.xlsx', '.pptx'],
+    'Images': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg'],
+    'Videos': ['.mp4', '.mkv', '.flv', '.avi', '.mov'],
+    'Music': ['.mp3', '.wav', '.aac', '.flac'],
+    'Archives': ['.zip', '.rar', '.tar', '.gz']
+}
+
+# Make a list of all the files in the given directory
+fileList = [files for files in os.listdir(filePath)]
+
+print(fileList)
